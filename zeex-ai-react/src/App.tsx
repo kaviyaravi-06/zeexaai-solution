9fe832165f426406757a8726f93c91417c6a00d4
kaviyaravi-06 <kaviyaravi831@gmail.com>
2025-11-29 16:39:30 +0530

    Initial commit - ZeexAI React project

---
+++ zeex-ai-react/src/App.tsx
@@ -0,0 +1,14 @@
+// App.tsx
+import React from 'react';
+import SolutionsGrid from './components/SolutionsGrid';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SolutionsGrid />
+    </div>
+  );
+}
+
+export default App;
